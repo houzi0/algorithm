9b9050871e89da29adc1c1b35dce4c7110810369
houzi <houzi@qq.com>
2022-12-19 14:50:20 +0800

    add leetcode

---
+++ leetcode/src/cn/lettcode/Main.java
@@ -0,0 +1,12 @@
+package cn.lettcode;
+
+/**
+ * @author houzi
+ * @Date 2022/12/19
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
