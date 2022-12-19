8d96fe0aeb5a4474ccb8cd116fe422d0a5ba04c3
houzi <houzi@qq.com>
2022-12-19 18:32:54 +0800

    1. 两数之和 ok 2. 两数相加 error

---
+++ leetcode/src/cn/lettcode/LT_1_两数之和/Main.java
@@ -0,0 +1,52 @@
+package cn.lettcode.LT_1_两数之和;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @author houzi
+ * @Date 2022/12/19
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/two-sum">1. 两数之和</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("result = " + Arrays.toString(result));
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                result[0] = map.get(nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(target - nums[i], i);
+        }
+        return result;
+    }
+}
